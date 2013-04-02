9cdf407b1f9e0ba85acb08b7cfcb564706b56a43
micha <trieba@e-gestaltung.de>
2013-04-02 21:44:31 +0200

    tidy up

--- app/js/filters.js
+++ app/js/filters.js
@@ -9,27 +9,7 @@ angular.module('myApp.filters', []).
      }
   }]).
   filter('interpolate', ['version', function(version) {
-        return function(text) {
-            return String(text).replace(/\%VERSION\%/mg, version);
-        }
+    return function(text) {
+        return String(text).replace(/\%VERSION\%/mg, version);
+    }
     }]);
-
-/*
- angular.module('your_app') // attach to your app or create a new with ('filters', [])
- .filter('isodate', function(){
- return function(datetime){
- var n = datetime.split(' ');
- if(n.length == 1) return datetime;
- else return n.join('T')+'Z';
- };
- });
-*/
-
-/*
- filter('interpolate', ['version', function(version) {
- return function(text) {
- return String(text).replace(/\%VERSION\%/mg, version);
- }
- }]).
-
-    */
